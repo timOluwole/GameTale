8e9073fbd34a234068f376f50a01761bd4ed64da
Timothy Oluwole <to-kaminari@hotmail.com>
2017-12-29 20:31:44 +0000

    Add files via upload
    All the C# code used in the game

---
+++ BallControl.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BallControl : MonoBehaviour {
+
+    SwitchMode mode;
+
+	//For Movement
+	public float speed;
+	private Rigidbody rb;
+
+	//For Jumping
+	public float jumpSpeed = 1000.0f;
+	private bool isFalling = false;
+
+	//For Scoring
+	private int count;
+	public Text countText;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		count = 5;
+		SetCountText ();
+
+	}
+
+	void Update() { 
+       
+		//Movement
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		rb.AddForce (movement * speed);
+
+        
+
+        //Jumping
+    
+        //Raccoon Mode
+        if (Input.GetKeyDown("space") && isFalling == false)
+		{
+            FindObjectOfType<AudioManager>().Play("Jump");
+            rb.AddForce(Vector3.up * jumpSpeed);
+
+            if (SwitchMode.hi() == 0)
+            {
+                isFalling = true;
+            }
+
+
+        }
+
+
+
+	}
+
+
+
+    
+
+
+
+
+
+	void SetCountText(){
+		countText.text = "Gold Needed: " + count.ToString ();
+	}
+
+    void SetWinText()
+    {
+        countText.text = "Find The Statue!";
+    }
+
+
+
+    void OnCollisionStay(){
+		isFalling = false;
+	}
+
+
+	void OnTriggerEnter(Collider other) 
+	{
+        if (other.gameObject.CompareTag("Statue"))
+        {
+            if (count == 0)
+            {
+                countText.text = "YOU WIN!!!";
+            }
+        }
+
+
+        if (other.gameObject.CompareTag("Water") && SwitchMode.hi() == 0)
+        {
+            isFalling = false;
+        }
+
+
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			count--;
+            other.gameObject.SetActive(false);
+            if (count == 0)
+            {
+                SetWinText();
+            }
+            else
+            {
+                SetCountText();
+            }
+
+			
+			
+			FindObjectOfType<AudioManager> ().Play ("Collect Coin");
+		}
+	}
+
+}
+
---
+++ CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    private int modeNo;
+    public GameObject raccoonMode;
+    public GameObject birdMode;
+    private GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        modeNo = 0;
+        player = raccoonMode;
+        offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+        if (Input.GetKeyUp("1")) {
+            if (modeNo == 0) {
+                modeNo = 1;
+                player = birdMode;
+            } else if (modeNo == 1) {
+                modeNo = 0;
+                player = raccoonMode;
+            }
+        }
+        transform.position = player.transform.position + offset;
+	}
+}
